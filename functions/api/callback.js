07d6bc19fbdf0d396aadbaab43f213dcb050d62f
xuanhung2598-web <xuanhung2598@gmail.com>
2025-10-15 15:01:25 +0700

    Update callback.js

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -1,22 +1,6 @@
-export async function onRequestGet({ request, env }) {
-  const url = new URL(request.url);
-  const code = url.searchParams.get("code");
-
-  const tokenResponse = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: { "Content-Type": "application/json", "Accept": "application/json" },
-    body: JSON.stringify({
-      client_id: env.GITHUB_CLIENT_ID,
-      client_secret: env.GITHUB_CLIENT_SECRET,
-      code,
-    }),
-  });
-
-  const tokenData = await tokenResponse.json();
-  return new Response(JSON.stringify(tokenData), {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  });
+export async function onRequestGet(context) {
+  // Khi GitHub trả về /api/callback?code=xxxx,
+  // Cloudflare sẽ tự xử lý token OAuth nội bộ.
+  // File này chỉ cần redirect người dùng về lại trang CMS.
+  return Response.redirect("https://wheeledit-website.pages.dev/admin/#/");
 }
