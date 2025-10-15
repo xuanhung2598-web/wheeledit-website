f96e93a2ca40049980df38fa5b85997d3acc6aa9
xuanhung2598-web <xuanhung2598@gmail.com>
2025-10-15 15:37:44 +0700

    s

--- functions/api/auth.js
+++
@@ -1,6 +0,0 @@
-export async function onRequestGet(context) {
-  const clientId = context.env.GITHUB_CLIENT_ID;
-  const redirectUri = "https://wheeledit-website.pages.dev/api/callback";
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=repo,user`;
-  return Response.redirect(githubAuthUrl);
-}
